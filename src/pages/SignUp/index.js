3010d9f748c909df00b3b2fd3f299594ff4f4a11
arthur-pian <arthurbessapian@hotmail.com>
2025-05-22 22:41:18 -0300

    first commit

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react';
+import { Platform } from 'react-native';
+
+import { 
+  Background, 
+  Container, 
+  AreaInput, 
+  Input, 
+  SubmitButton, 
+  SubmitText
+} from '../SignIn/styles';
+
+import { AuthContext } from '../../contexts/auth';
+
+
+export default function SignUp(){
+
+  const { user } = useContext(AuthContext)
+
+  function handleSignUp(){
+    console.log(user);
+  }
+
+  return(
+    <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
+      >
+
+        <AreaInput>
+          <Input
+            placeholder="Nome"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Seu email"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Sua senha"
+          />
+        </AreaInput>
+
+        <SubmitButton onPress={handleSignUp}>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+
+      </Container>
+
+    </Background>
+  )
+}
